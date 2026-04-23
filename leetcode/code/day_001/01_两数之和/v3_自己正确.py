f0095333b3cdc19f1ed4894a6959152822e9e2c1
WanRui37 <1446145652@qq.com>
2026-04-23 16:20:04 +0800

    leetcode day_001

---
+++ leetcode/code/day_001/01_两数之和/v3_自己正确.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target-num not in hashtable:
+                hashtable[num] = i
+            else:
+                return [hashtable[target-num], i]
+        return []
