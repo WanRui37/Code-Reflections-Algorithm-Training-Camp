d0775cdc94fc0efdbca8e3e0f63435dc615a31e9
WanRui37 <1446145652@qq.com>
2026-04-24 17:16:42 +0800

    update day_038

--- code/day_038/03_单词拆分/v1_随想录.py
+++ code/day_038/03_单词拆分/v1_随想录.py
@@ -1,21 +1,14 @@
 class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [0] * (target + 1)
-        dp[0] = 1
-        for i in range(1, target + 1):  # 遍历背包
-            for j in range(len(nums)):  # 遍历物品
-                if i - nums[j] >= 0:
-                    dp[i] += dp[i - nums[j]]
-        return dp[target]
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)  # dp[i] 表示字符串的前 i 个字符是否可以被拆分成单词
+        dp[0] = True  # 初始状态，空字符串可以被拆分成单词
 
-class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [0] * (target + 1)  # 创建动态规划数组，用于存储组合总数
-        dp[0] = 1  # 初始化背包容量为0时的组合总数为1
+        for i in range(1, n + 1): # 遍历背包
+            for j in range(i): # 遍历单词
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True  # 如果 s[0:j] 可以被拆分成单词，并且 s[j:i] 在单词集合中存在，则 s[0:i] 可以被拆分成单词
+                    break
 
-        for i in range(1, target + 1):  # 遍历背包容量
-            for j in nums:  # 遍历物品列表
-                if i >= j:  # 当背包容量大于等于当前物品重量时
-                    dp[i] += dp[i - j]  # 更新组合总数
-
-        return dp[-1]  # 返回背包容量为target时的组合总数
+        return dp[n]
